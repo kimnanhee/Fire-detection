85dd30c1f3b6e8c6d37c100186aae1dc652cbebf
kimnanhee <kimnanhee0225@gmail.com>
2020-09-29 13:48:36 +0900

    [Modefien] sensor.h

--- 30/main.c
+++ 30/main.c
@@ -8,6 +8,8 @@ int main(void)
 {
 	DDRF = 0x00; // 가스, 불꽃, 온도센서 입력핀
     DDRC = 0xFF; // 부저, 팬모터, 릴레이 출력핀
+	PORTC = 0x04; // 평상시 포트 출력
+	int state = 0;
 	
     while (1) 
     {
@@ -15,6 +17,32 @@ int main(void)
 		int gas_state = gas_sensor_read();
 		int fire_state = fire_sensor_read();
 		
+		if(temp_state==1 && gas_state==1 && fire_state==1) state = 4; // 화재
+		else if(temp_state==1 && gas_state==0 && fire_state==0) state = 1; // 온도
+		else if(temp_state==0 && gas_state==1 && fire_state==0) state = 2; // 가스
+		else if(temp_state==0 && gas_state==0 && fire_state==1) state = 3; // 불꽃
+		else state = 0; // 평상
+		
+		switch(state)
+		{
+			case 0: // 평상
+			PORTC = 0x04;
+			break; 
+			
+			case 1: // 온도 or 가스 이상
+			case 2:
+			PORTC = 0x06;
+			break;
+			
+			case 3: // 불꽃 이상
+			PORTC = 0x04;
+			SERVO();
+			break;
+			
+			case 4: // 화재
+			PORTC = 0x01;
+			break;
+		}
 		_delay_ms(500); // 0.5초마다 측정
     }
 }
--- 30/sensor.h
+++ 30/sensor.h
@@ -1,20 +1,48 @@
 ﻿#ifndef SENSOR_H_
 #define SENSOR_H_
 
+void ADC_set() // ADC 설정
+{
+	ADCSRA = (1 << ADEN); // ADC enable
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 16Mhz/128 = 125Khz
+}
+
+uint16_t ADC_read(uint8_t channel) // channel에 해당하는 ADC값 반환
+{
+	ADMUX = (1 << REFS0); // ADC0, 5V에 0~1023 설정
+	ADMUX |= channel; // ADC핀 설정
+	
+	ADCSRA |= (1 << ADSC); // ADC 변환 시작
+	while(ADCSRA & (1 << ADSC)); // ADC 변환 완료
+	
+	return ADCW;  // Return converted value
+}
+
 int temp_sensor_read() // 온도가 일정 수준을 넘으면 1을 반환
 {
-	char key = PINF & 0x02;
-	if(key == 0x02) return 1;
+	ADC_set();
+	int value = ADC_read(0);
+	
+	float temp = (float)value * 5.0 / 1024 * 100; // ~`C 단위의 온도
+	
+	if(temp > 36) return 1; // 36도 이상일때 1 반환
+	else return 0;
 }
+
 int gas_sensor_read() // 가스이 일정 수준을 넘으면 1을 반환
 {
-	char key = PINF & 0x02;
-	if(key == 0x02) return 1;
+	ADC_set();
+	int value = ADC_read(2);
+	
+	if(value > 500) return 1;
+	else return 0;
 }
+
 int fire_sensor_read() // 불꽃이 감지되면 1을 반환
 {
 	char key = PINF & 0x02;
 	if(key == 0x02) return 1;
+	else return 0;
 }
 
 #endif /* SENSOR_H_ */
